12966377c956502ddf254b6d0acbfb6108d8cb44
Ethan Way <ethan.way@comcast.net>
2024-08-27 15:24:41 -0600

    saved page

---
+++ app/src/main/java/com/example/myapplication/utils/NavGraph.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.utils
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.myapplication.ui.MainScreen
+import com.example.myapplication.ui.SavedScreen
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen(navController) }
+        composable("savedScreen") { SavedScreen(navController) }
+    }
+}
