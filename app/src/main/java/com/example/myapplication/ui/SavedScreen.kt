292d628231688bf36df930b569c5d7b8a9007466
Ethan Way <ethan.way@comcast.net>
2024-09-05 11:07:39 -0600

    ui changes

--- app/src/main/java/com/example/myapplication/ui/SavedScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/SavedScreen.kt
@@ -1,17 +1,7 @@
 package com.example.myapplication.ui
 
-import android.content.DialogInterface
-import android.content.Intent
+import android.app.Activity
 import androidx.compose.material3.Icon
-import android.graphics.drawable.ColorDrawable
-import android.net.Uri
-import android.text.Html
-import android.text.method.LinkMovementMethod
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -26,22 +16,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.example.myapplication.R
 import com.example.myapplication.data.AppDatabase
 import com.example.myapplication.data.Record
 import com.example.myapplication.data.RecordDao
 import com.example.myapplication.utils.parseRecord
-import com.example.myapplication.utils.setRatingStars
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -52,6 +40,17 @@ import kotlinx.coroutines.withContext
 @Composable
 fun SavedScreen(navController: NavController) {
     val context = LocalContext.current
+    val activity = context as? Activity
+    val view = LocalView.current
+
+    LaunchedEffect(Unit) {
+        activity?.window?.let { window ->
+            WindowCompat.getInsetsController(window, view).apply {
+                window.statusBarColor = Color(51, 51, 51).toArgb()
+            }
+        }
+    }
+
     val db = remember { AppDatabase.getDatabase(context) }
     val recordDao: RecordDao = db.recordDao()
     var records by remember { mutableStateOf<List<Record>>(emptyList()) }
