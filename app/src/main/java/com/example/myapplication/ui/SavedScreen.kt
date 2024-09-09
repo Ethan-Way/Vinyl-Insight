be3b8f17cd8b50d3cb94168569d50dab83dc4c15
Ethan Way <ethan.way@comcast.net>
2024-09-09 13:55:57 -0600

    clean up + making button consistent

--- app/src/main/java/com/example/myapplication/ui/SavedScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/SavedScreen.kt
@@ -11,13 +11,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.Sort
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
